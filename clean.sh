bf742290621ff6199de7b4ec0d92c07c1d742f0b
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2012-08-09 14:31:58 +0300

    build: Unmanage config.h header file

--- clean.sh
+++ clean.sh
@@ -2,9 +2,9 @@
 
 rm -f aclocal.m4 compile configure
 rm -rf autom4te.cache/
-rm -rf config
+rm -rf config config.*
 
-find ./ -type f -name "config.h.in*" -delete;
+find ./ -type f -name "config.h*" -delete;
 find ./ -type f -name "Makefile.in" -delete;
 find ./ -type f -name "*.xml" -delete;
 find ./ -type f -name "*.gcno" -delete;
--- src/event.c
+++ src/event.c
@@ -18,8 +18,6 @@
  *
  */
 
-#include <config.h>
-
 #include <connline/event.h>
 #include <connline/private.h>
 
